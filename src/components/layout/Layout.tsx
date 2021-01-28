7e2686e5c10a7d8d082a1a08d51ba003fe19098c
felis <felismargarita@hotmail.com>
2021-01-28 10:14:46 +0800

    完成layout

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default ()=>{
+
+
+  return <>
+  
+  <div className="layout-container">
+    <div className="side-bar">
+      123
+    </div>
+    <div className="middle-bar">
+      456
+    </div>
+    <div className="right-bar">
+      789
+    </div>
+  </div>
+  </>
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '@/components/layout/Layout'
+import '@/style/style.scss'
+export default function IndexPage() {
+  return (
+    <div>
+      <Layout/>
+    </div>
+  );
+}
